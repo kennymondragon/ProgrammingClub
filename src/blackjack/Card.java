dd46e61e37562fc69334742ff33e72aefe3b5e33
Rileyjs <swansonr5@my.normandale.edu>
2017-01-23 20:02:09 -0600

    Cleaning

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -1,32 +1,29 @@
 package blackjack;
 
-public class Card 
-{
+public class Card {
 	private int value;
 	private String face;
 	private String suit;
-	
-	public Card(int val, String s,String f)
-	{
+
+	public Card(int val, String s, String f) {
 		this.value = val;
 		this.face = f;
 		this.suit = s;
 	}
-	
-	public int getValue()
-	{
+
+	public int getValue() {
 		return value;
 	}
-	public String getSuit()
-	{
+
+	public String getSuit() {
 		return suit;
 	}
-	public String getFace()
-	{
+
+	public String getFace() {
 		return face;
 	}
-	public String toString()
-	{
+
+	public String toString() {
 		return face + suit;
 	}
 }
