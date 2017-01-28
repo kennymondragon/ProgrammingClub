c8ede87763fa3ecaf945df55c2abde1036bf0617
mechanic64 <thor2980@yahoo.com>
2017-01-27 18:52:25 -0600

    Update with Player class, 1-27-17

---
+++ Card.java
@@ -0,0 +1,35 @@
+//Ben Olson
+//PC
+
+//package blackjack;
+
+public class Card 
+{
+	private int value=0;
+	private String face;
+	private String suit;
+	
+	public Card(int val, String s,String f)
+	{
+		this.value = val;
+		this.face = f;
+		this.suit = s;
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	public String getSuit()
+	{
+		return suit;
+	}
+	public String getFace()
+	{
+		return face;
+	}
+	public String toString()
+	{
+		return face + suit;
+	}
+}
